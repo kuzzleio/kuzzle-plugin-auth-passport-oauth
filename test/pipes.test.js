f7d14bdbb44f95cf23a66fd703d010b17c843474
jenow <jenova.ke20@gmail.com>
2016-02-03 10:51:21 +0100

    merge

---
+++ test/pipes.test.js
@@ -0,0 +1,26 @@
+var
+  should = require('should'),
+  pipes = require('../lib/config/pipes'),
+  rewire = require('rewire'),
+  PluginGithub = rewire('../lib');
+
+describe('pipes definition', function() {
+  var pluginGithub;
+
+  before(function () {
+    pluginGithub = new PluginGithub();
+    pluginGithub.init({persist: true, clientID: 'id', clientSecret: 'secret', callbackUrl: 'http://callback.url', scope: ['test']});
+  });
+
+  it('should link kuzzle pipe correctly', function() {
+    should(pipes).be.an.Object();
+    should(pipes["passport:loadScope"]).be.a.String().and.be.eql('loadScope');
+  });
+
+  it('should trigger loadScope pipe', function(done) {
+    pluginGithub.loadScope({scope: {}}, function(err, object) {
+      should(object.github[0]).equal('test');
+      done();
+    });
+  });
+});

6c138e9a9aee4e8229aa709f2b9cb5c079a813b1
jenow <jenova.ke20@gmail.com>
2016-02-03 16:25:55 +0100

    Tests

--- test/verify.test.js
+++ test/verify.test.js
@@ -3,14 +3,46 @@ var
   pipes = require('../lib/config/pipes'),
   rewire = require('rewire'),
   PluginGithub = rewire('../lib'),
-  strategy = require('../lib/passport/strategy'),
-  proxyquire = require('proxyquire');
-
+  Strategy = require('../lib/passport/strategy'),
+  proxyquire = require('proxyquire'),
+  q = require('q');
 
 describe('passport verify', function() {
   var
     pluginGithub,
-    Passport;
+    Passport,
+    isCalled = false,
+    config = {
+      "persist": [
+        "login"
+      ],
+      "scope": [
+        "user:email"
+      ],
+      "clientID": "<your-client-id>",
+      "clientSecret": "<your-client-secret>",
+      "callbackUrl": "http://host:7511/api/1.0/_login/github"
+    },
+    context = {
+      repositories: function() {
+        return {
+          user: {
+            ObjectConstructor: function () {
+              return {};
+            },
+            load: function () {
+              return q(null);
+            },
+            persist: function() {
+              isCalled = true;
+            },
+            hydrate: function() {
+              return q({});
+            }
+          }
+        };
+      }
+    };
 
   before(function () {
     pluginGithub = new PluginGithub();
@@ -29,8 +61,17 @@ describe('passport verify', function() {
     });
   });
 
-  it('should load a new user', function() {
+  it('should persist and load a new user', function() {
     Passport = proxyquire('passport', {});
-    strategy.verify();
+    Strategy = new Strategy(context);
+    Strategy.load(Passport, config);
+    Strategy.verify("accessToken", "refreshToken", {
+      _json: {
+        login: "login"
+      }
+    }, function() {}).then(function(response) {
+      should(response.login).be.a.String().and.be.eql('login');
+      should(isCalled).equal(true);
+    });
   });
 });

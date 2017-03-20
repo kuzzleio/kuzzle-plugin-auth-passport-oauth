092a5c7152085b018f16a171dfc2856bf219c140
David Bengsch <dbengsch@kaliop.com>
2017-03-20 16:35:37 +0100

    Fix unit-test and other boyscoutisms

--- test/init.test.js
+++ test/init.test.js
@@ -1,31 +1,29 @@
-var
+const
   should = require('should'),
   rewire = require('rewire'),
   sinon = require('sinon'),
   PluginOAuth = rewire('../lib');
 
 describe('The plugin oauth initialization', function () {
-
-  var pluginOAuth;
+  let pluginOAuth;
 
   before(function () {
     pluginOAuth = new PluginOAuth();
   });
 
   it('should return an error if strategies config is empty', function() {
-    var warnSpy = sinon.spy();
+    const warnSpy = sinon.spy();
     return PluginOAuth.__with__({
       console: {
         warn: warnSpy
       }
     })(() => {
       pluginOAuth.init({});
-      should(warnSpy.calledOnce).be.true;
+      should(warnSpy.calledOnce).be.true();
     });
   });
 
   it('should return pluginOauth object if everything is ok', function () {
     should(pluginOAuth.init({persist: true, strategies: [{name: "facebook", clientID: "id", clientSecret: "secret", callbackUrl: "http://callback.url"}]})).be.Object();
   });
-
 });

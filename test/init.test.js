5e1ca4c9920ebed1a06ed9165aa3b95e0879aac6
jenow <jenova.ke20@gmail.com>
2017-05-03 14:15:56 +0200

    fixes tests

--- test/init.test.js
+++ test/init.test.js
@@ -8,33 +8,48 @@ const
     }
   });
 
-describe('#init', function () {
+describe('#init', () => {
   let
     pluginOauth,
-    pluginContext = require('./mock/pluginContext.mock.js');
+    pluginContext = require('./mock/pluginContext.mock.js'),
+    config = {
+      strategies: {
+        facebook: {
+          credentials: {
+            clientId: 'clientId',
+            secret: 'secret'
+          }
+        }
+      }
+    };
 
-  beforeEach(function () {
+  beforeEach(() => {
     sandbox.reset();
     pluginOauth = new PluginOAuth();
   });
 
-  it('should return a Promise', function () {
-    should(pluginOauth.init(null, pluginContext)).be.a.Promise();
+  it('should reject if no credentials are specified', () => {
+    return should(pluginOauth.init({strategies: {facebook: {}}}, pluginContext)).be.rejectedWith(`Error loading strategy [facebook]: no credentials provided`);
   });
 
-  it('should initialize all strategies', function (done) {
-    should(pluginOauth.strategies).not.be.ok();
+  it('should reject if the strategy does not exists', () => {
+    return should(pluginOauth.init({
+      strategies: {
+        fake: {
+          credentials: {
+            clientId: 'clientId',
+            secret: 'secret'
+          }
+        }
+      }
+    }, pluginContext)).be.rejectedWith(`Error loading strategy [fake]: Cannot find module 'passport-fake'`);
+  });
 
-    pluginOauth.init(null, pluginContext)
+  it('should initialize all strategies', done => {
+    pluginOauth.init(config, pluginContext)
       .then(() => {
         should(pluginOauth.strategies).be.Object();
         done();
       });
-  });
-
-  it('should have a getUserRepository method returning an object', function() {
-    pluginOauth.init(null, pluginContext);
-
-    should(pluginOauth.getProviderRepository('provider')).be.an.Object();
   });
 });
